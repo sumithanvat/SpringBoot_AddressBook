4fb8d2cb5faa072647110e3fcd6234d8ccf0e397
Sumithanvat <skhanvat@gmail.com>
2023-07-26 00:52:39 +0530

    AddedValidation and costomException is added

--- src/main/java/com/bridgelabz/AddressBookSpringBook/dto/ContactDTO.java
+++ src/main/java/com/bridgelabz/AddressBookSpringBook/dto/ContactDTO.java
@@ -1,20 +1,25 @@
 package com.bridgelabz.AddressBookSpringBook.dto;
 
 import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
+import lombok.Data;
 
 import java.util.List;
-
+@Data
 public class ContactDTO {
     @Pattern(regexp = "^[A-Z]{1}[a-z]{2,}$",message ="Name not valid")
     public String name;
     @Pattern(regexp ="^[+]{1}[0-9]{2}[6-9]{1}[0-9]{9}$",message = "Invalid phone number")
     public String phoneNumber;
+    @NotEmpty
     public String email;
+    @NotEmpty
     public List<String> address;
     @NotEmpty
     public String state;
     @NotEmpty
     public String city;
+    @NotNull
     public long pinCode;
 }
